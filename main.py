c34ebfa05de81a7c71602d258c3e22763adc4ef1
kcarpe28 <kcarpe28@gmail.com>
2026-01-05 22:37:56 -0500

    added dt variable to track time between refreshes

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    gameclock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         log_state()
 
@@ -15,10 +20,10 @@ def main():
         screen.fill("black")
         pygame.display.flip()
         
-
-
-
-
+        gameclock.tick(60)
+        dt = gameclock.tick_busy_loop()/1000
+        
+        
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f'Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}')
